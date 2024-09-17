ddcf90c8f91249189b00d137739dfa955950e963
Glevinzon Dapal <glevinzond@gmail.com>
2024-09-18 00:42:11 +0800

    chore: display content body

--- src/app/components/content/content.tsx
+++ src/app/components/content/content.tsx
@@ -2,16 +2,23 @@ import React, { FC } from "react";
 import Image from "next/image";
 import { Article } from "./contentTypes";
 
-const Content: FC<Article> = ({ featuredImage, author, title, subtitle }) => {
+const Content: FC<Article> = ({
+  featuredImage,
+  author,
+  title,
+  subtitle,
+  body,
+  conclusion,
+}) => {
   return (
     <div className="flex flex-col">
       <div className="flex flex-row">
-        <div className="flex flex-1 flex-col gap-4 justify-center px-[64px]">
-          <p className="text-md text-brand-700 font-semibold">
+        <div className="flex flex-1 flex-col justify-center px-[5rem]">
+          <p className="text-md text-brand-700 font-semibold mb-4">
             20 Jan 2024 • 10 min read
           </p>
-          <p className="display-lg font-semibold">{title}</p>
-          <p className="text-xl font-normal text-tertiary w-[480px]">
+          <p className="display-lg font-semibold mb-3">{title}</p>
+          <p className="text-xl font-normal text-tertiary w-[480px] mb-12">
             {subtitle}
           </p>
           <div className="flex gap-4">
@@ -42,7 +49,80 @@ const Content: FC<Article> = ({ featuredImage, author, title, subtitle }) => {
           )}
         </div>
       </div>
-      <div className="flex">body</div>
+      <div className={`flex flex-col w-[720px] mx-auto py-24`}>
+        {body.map((element, index) => {
+          switch (element.type) {
+            case "p":
+              return (
+                <p
+                  key={index}
+                  className="text-xl font-normal text-tertiary mb-5"
+                >
+                  {element.content}
+                </p>
+              );
+            case "divider":
+              return (
+                <div key={index} className="border-b border-secondary my-8" />
+              );
+            case "h2":
+              return (
+                <h2
+                  key={index}
+                  className="display-sm font-semibold mb-5 text-primary"
+                >
+                  {element.content}
+                </h2>
+              );
+            case "img":
+              return (
+                <>
+                  <Image
+                    key={index}
+                    width={500}
+                    height={500}
+                    src={element.url || ""}
+                    alt={element.caption || ""}
+                    className="w-full h-auto mb-5 rounded-xl"
+                  />
+                  <div className="text-sm font-normal text-tertiary mb-[3rem]">
+                    {element.caption}
+                  </div>
+                </>
+              );
+            case "quote":
+              return (
+                <>
+                  <blockquote
+                    key={index}
+                    className="display-xs italic font-bold mb-4"
+                  >
+                    “{element.content}”
+                  </blockquote>
+                  <p className="text-md font-normal mb-[3rem]">
+                    — {element.author}
+                  </p>
+                </>
+              );
+            case "h3":
+              return (
+                <h3 key={index} className="text-2xl font-bold mb-5">
+                  {element.content}
+                </h3>
+              );
+            default:
+              return null;
+          }
+        })}
+        <div className="flex flex-col rounded-2xl bg-secondary p-8 mt-[3rem]">
+          <div className="display-sm font-semibold text-primary mb-5 ">
+            Conclusion
+          </div>
+          <div className="text-lg font-normal text-tertiary prose">
+            {conclusion}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,20 +27,57 @@ const config: Config = {
           900: "rgba(66, 48, 125, 1)",
           950: "rgba(44, 28, 95, 1)",
         },
+        primary: {
+          DEFAULT: "rgba(16, 24, 40, 1)",
+        },
+        secondary: {
+          DEFAULT: "rgba(228, 231, 236, 1)",
+        },
         tertiary: {
           DEFAULT: "rgba(71, 84, 103, 1)",
         },
       },
-      typography: {},
+      spacing: {
+        none: "0rem",
+        xxs: ".125rem",
+        xs: ".25rem",
+        sm: ".375rem",
+        md: ".5rem",
+        lg: "0.75rem",
+        xl: "1rem",
+        "2xl": "1.25rem",
+        "3xl": "1.5rem",
+        "4xl": "2rem",
+        "5xl": "2.5rem",
+        "6xl": "3rem",
+        "7xl": "4rem",
+        "8xl": "5rem",
+        "9xl": "6rem",
+        "10xl": "8rem",
+        "11xl": "10rem",
+      },
     },
   },
   plugins: [
+    require("@tailwindcss/typography"),
     plugin(function ({ addComponents }) {
       addComponents({
+        ".display-xs": {
+          fontSize: "1.5rem",
+          lineHeight: "1.875rem",
+        },
+        ".display-sm": {
+          fontSize: "1.875rem",
+          lineHeight: "2.375rem",
+        },
         ".display-lg": {
           fontSize: "3rem",
           lineHeight: "3.75rem",
           letterSpacing: "-2%",
+        },
+        ".text-sm": {
+          fontSize: ".875rem",
+          lineHeight: "1.25rem",
         },
         ".text-md": {
           fontSize: "1rem",
